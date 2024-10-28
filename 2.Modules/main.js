9c3c41a66f932296df4aa4c53afd6c1e71f3ea1e
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-28 18:35:47 +0100

    CommonJS modules

---
+++ 2.Modules/main.js
@@ -0,0 +1,5 @@
+const { add, addAndMultiply } = require('./functions/add')
+const subtract = require('./functions/subtract')
+console.log(add(1, 2))
+console.log(addAndMultiply(1, 2, 5))
+console.log(subtract(50, 2))
