f8f5b43c902bdba64a7a2dc63eefa7fc5c49111e
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-11-01 23:56:01 +0100

    Dev server and setup paramerts

---
+++ 8.3 Course DevServer /webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    courses: './src/pages/courses.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                quietDeps: true,
+              },
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html',
+    }),
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, './src/pages/courses.html'),
+      inject: true,
+      chunks: ['courses'],
+      filename: 'pages/courses.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: path.join(__dirname, './src/assets'), to: 'assets' }],
+    }),
+  ],
+}
